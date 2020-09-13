0ae9c9926bf5d7ba18f47d0dba5f4ef4c812e85e
Sithi Nasila <Sithi.nasila@gmail.com>
2020-09-14 00:15:44 +0530

    initial commit

---
+++ clone/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Countdown from './components/Countdown';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Countdown></Countdown>
+    </div>
+  );
+}
+
+export default App;
